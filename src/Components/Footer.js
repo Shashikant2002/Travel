a766a27c14b87f6031882459b516c3ec3bd29545
Shashikant Prajapti <87322423+Shashikant2002@users.noreply.github.com>
2022-07-11 09:43:48 +0000

    xom

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,9 +3,10 @@ import React from 'react'
 const Footer = () => {
     return (
         <div className='footer'>
-            <div className="container">
-                <div className="connected flex">
-                    
+            <div className="container flex">
+                <div className="connected">
+                    <h4>Newsletter Sign Up</h4>
+                    <p>Sign Up for News And Special Offers</p>
                 </div>
             </div>
         </div>

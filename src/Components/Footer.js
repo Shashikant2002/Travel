84a369ffe989b5c8279de4ae6c74671d1f00968d
Shashikant Prajapti <87322423+Shashikant2002@users.noreply.github.com>
2022-08-30 12:28:18 +0000

    jhj

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,7 +3,13 @@ import React from 'react'
 const Footer = () => {
     return (
         <div className='footer'>
-            <div className="container flex">
+            <div className="container flex just_between"> 
+                <div className="connected">
+                    <h4>Newsletter Sign Up</h4>
+                    <p>Sign Up for News And Special Offers</p>
+                    <input type="email" placeholder='Enter Email Address'/>
+                    <button class="glo_btn">Subscribe</button>
+                </div>
                 <div className="connected">
                     <h4>Newsletter Sign Up</h4>
                     <p>Sign Up for News And Special Offers</p>
